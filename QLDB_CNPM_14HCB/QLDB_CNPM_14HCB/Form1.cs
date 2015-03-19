568419d982d9bd2022223597f0fb062acc3ef7dd
Le Quy Kieu Hung <kieuhunglkdn@gmail.com>
2015-03-19 18:31:33 +0700

    Tao moi project

---
+++ QLDB_CNPM_14HCB/QLDB_CNPM_14HCB/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QLDB_CNPM_14HCB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
